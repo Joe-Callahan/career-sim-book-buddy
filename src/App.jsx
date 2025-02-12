76a544f70e45c2bc2f4a211fe93f96d3dfec8312
Joe Callahan <joecallahan@macbookair.lan>
2025-02-12 11:05:44 -0800

    login and register pages produce tokens

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,28 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Link } from 'react-router-dom'
 import { useState } from 'react'
 import Home from './components/Home.jsx'
 import BookDetails from './components/BookDetails.jsx'
-
-
+import Register from './components/Register.jsx'
+import LogIn from './components/LogIn.jsx'
 
 const App = () => {
   const [selectedBook, setSelectedBook] = useState({});
+  const [token, setToken] = useState('');
 
-  console.log(selectedBook);
+  console.log(`token = ${token}`);
   
   return (
     <>
       <h1>Book Buddy</h1>
+      <nav>
+        <Link to='/register'>Sign Up</Link>
+      </nav>
 
       <Routes>
         <Route path='/' element={<Home setSelectedBook={setSelectedBook} />} />
         <Route path='/books/:id' element={<BookDetails selectedBook={selectedBook} />} />
+        <Route path='/register' element={<Register setToken={setToken} />} />
+        <Route path='/login' element={<LogIn setToken={setToken} />} />
       </Routes>
     </>
   )
---
+++ src/components/LogIn.jsx
@@ -0,0 +1,50 @@
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+
+const LogIn = (props) => {
+  const [loginEmailInput, setLoginEmailInput] = useState('');
+  const [passwordInput, setPasswordInput] = useState('');
+
+  const logIn = async(e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch ('https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/users/login', {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          email: loginEmailInput,
+          password: passwordInput
+        })
+      });
+      const userObj = await response.json();
+      props.setToken(userObj.token);
+    } catch(err) {
+      alert(err);
+    }
+  }
+
+  return (
+    <>
+      <h1>Log In</h1>
+      <form onSubmit={logIn}>
+        <input 
+          placeholder='email'
+          type='email'
+          onChange={(e) => {setLoginEmailInput(e.target.value)}}/>
+        <input 
+          placeholder='password'
+          type='password'
+          onChange={(e) => {setPasswordInput(e.target.value)}} />
+        <button>Sign In</button>
+      </form>
+      <h4>New to Book Buddy?</h4>
+      <Link to='/register'>Create An Account Here</Link>
+    </>
+  )
+}
+
+
+
+export default LogIn
