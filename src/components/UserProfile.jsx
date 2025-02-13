aa9892900297e767c366a99c5c1e7bedc6b48105
Joe Callahan <joecallahan@macbookair.lan>
2025-02-13 11:58:46 -0800

    can view checked out books and check back in

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -2,6 +2,18 @@ import { useState, useEffect } from 'react';
 
 const UserProfile = (props) => {
   const [userInfo, setUserInfo] = useState({});
+  const [checkedOutBooks, setCheckedOutBooks] = useState([]);
+
+  const getReservations = async() => {
+    const response = await fetch('https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/reservations', {
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${props.token}`
+      }
+    })
+    const reservationArr = await response.json();
+    setCheckedOutBooks(reservationArr.reservation);
+  }
 
   useEffect(() => {
     const getProfileInfo = async () => {
@@ -15,8 +27,29 @@ const UserProfile = (props) => {
       setUserInfo(userInfoObj);
     }
     getProfileInfo();
-  })
-  
+  }, [])
+
+  useEffect(() => {
+    getReservations();
+  }, [])
+
+  const returnBook = async(e) => {
+    try {
+      const response = await fetch(`https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/reservations/${e.target.value}`, {
+      method: 'DELETE',
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${props.token}`
+      }
+      })
+      const returnObj = await response.json();
+      alert(`Return successful. Thank you!`);
+      getReservations();
+    } catch(err) {
+      alert(err);
+    }
+  }
+
   return (
     <>
       <h2>Profile Info</h2>
@@ -26,7 +59,25 @@ const UserProfile = (props) => {
               <section>
                 <p>{`Name: ${userInfo.firstname} ${userInfo.lastname}`}</p>
                 <p>{`Email: ${userInfo.email}`}</p>
-                <p>Books Checked Out:</p>
+                <p>{`Books Checked Out: ${checkedOutBooks.length}`}</p>
+                <ul>
+                  {
+                    checkedOutBooks.map((singleBook) => {
+                      return ( 
+                        <>
+                          <li key={singleBook.coverimage}>
+                            {singleBook.title}</li>
+                          <button 
+                            key={singleBook.id}
+                            value ={singleBook.id}
+                            onClick={returnBook}>
+                              Return This Book
+                          </button>
+                        </>
+                      )
+                    })
+                  }
+                </ul>
               </section>
             ) : (
               <h3>User Info Not Found. Please sign in or create a profile!</h3>
